24d2a8b672b236b8bdb94e6f601b35399162e2ea
ViktorGetman <123113442+ViktorGetman@users.noreply.github.com>
2023-02-07 23:29:52 +0300

    Внесены правки из юнита 3.2

--- skillfactory modul3/Program.cs
+++ skillfactory modul3/Program.cs
@@ -3,10 +3,15 @@
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Витя";
-
+        const string myName = "Витя";
+    
         Console.WriteLine(myName);
+
+        Console.WriteLine("\tПривет, мир");
+        Console.WriteLine("\tМне 28 лет");
+        Console.WriteLine("\tМеня зовут \nВитя");
+        Console.WriteLine("\x23");
+
 
         Console.ReadKey();
     }
